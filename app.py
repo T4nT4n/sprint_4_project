6b29ede821f68cb8007e871be61a2195eba3c5db
T4nT4n <tannerlawson99@gmail.com>
2024-09-13 22:15:48 -0600

    fourth commit?

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import datetime
 
 vehicle_df = pd.read_csv('vehicles_us.csv')
 
 # Add a header
 st.header("Exploratory Data Analysis")
 
-#Since I only want the make of a vehicle and not the model, I am going to filter out the model column that truly only contains the make--not including the model
-make_vehicle = vehicle_df.copy()
+#Let's fill the null values of the model year column with zero and convert to integer type
+vehicle_df['model_year'] = vehicle_df['model_year'].fillna(0).astype(int)
 
-#For this problem, we only care about the model and transmission columns so we should keep only these columns; we also filter for manual transmissions
-make_vehicle = make_vehicle.loc[make_vehicle['transmission'] == 'manual', ['model', 'transmission']]
+#Let's fill the null values of the cylinders column with zero
+vehicle_df['cylinders'] = vehicle_df['cylinders'].fillna(0).astype(int)
 
-#We filter the model the so that the make is the only thing in the model column
+#Let's fill the null values of the odometer column with -1
+vehicle_df['odometer'] = vehicle_df['odometer'].fillna(-1)
+
+#Let's fill in the null values within the paint color column to be filled in with "unknown"
+vehicle_df['paint_color'] = vehicle_df['paint_color'].fillna('unknown')
+
+#Let's fill in the null values in the 4wd column with 0. 1 represents True and 0 respresents False
+vehicle_df['is_4wd'] = vehicle_df['is_4wd'].fillna(0).astype(int)
+
+#Let's convert the date_posted column into datetime type
+vehicle_df['date_posted'] = pd.to_datetime(vehicle_df['date_posted'])
+
+#We are going to filter out the dataframe where the model year is 2000 or later, and since we filter to include model year, model, and odometer.
+filtered_vehicle_df = vehicle_df.loc[(vehicle_df['model_year'] >= 2000) & (vehicle_df['odometer'] != -1), ['model_year', 'model', 'odometer']]
+
+#We filter out the models of the cars, and keeping solely the make of the car
+make_vehicle = vehicle_df[['model', 'transmission']].copy()
 make_vehicle['model'] = make_vehicle['model'].str.split().str[0]
 
-#Now we check to see which models offer the most amount of manual transmissions
-grouped_make = make_vehicle.groupby('model').show()
+#Now we are going to group by the model of the vehicle and count the number of occurence that appear in the model index
+grouped_make = make_vehicle[make_vehicle['transmission'] == 'manual'].groupby('model').size().sort_values(ascending=False)
 
 #Create histogram
 fig_histogram = px.histogram(make_vehicle, x='model', title='The Number of Manual Transmission Offered by Makers of Vehicles', labels={'model': 'Car Model', 'count': 'Number of Available Manual Transmissions'})
