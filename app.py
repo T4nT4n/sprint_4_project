04564e248229780e0b8f724d5e04faf452208a1a
T4nT4n <tannerlawson99@gmail.com>
2024-09-14 08:31:38 -0600

    third submission

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import pandas as pd
 import plotly.express as px
 import datetime
 
+#Reading the CSV file
 vehicle_df = pd.read_csv('vehicles_us.csv')
 
-# Add a header
+# Adding a header
 st.header("Exploratory Data Analysis")
+
+"""Formatting the CSV File"""
 
 #Let's fill the null values of the model year column with zero and convert to integer type
 vehicle_df['model_year'] = vehicle_df['model_year'].fillna(0).astype(int)
@@ -26,18 +29,19 @@ vehicle_df['is_4wd'] = vehicle_df['is_4wd'].fillna(0).astype(int)
 #Let's convert the date_posted column into datetime type
 vehicle_df['date_posted'] = pd.to_datetime(vehicle_df['date_posted'])
 
-#We are going to filter out the dataframe where the model year is 2000 or later, and since we filter to include model year, model, and odometer.
-filtered_vehicle_df = vehicle_df.loc[(vehicle_df['model_year'] >= 2000) & (vehicle_df['odometer'] != -1), ['model_year', 'model', 'odometer']]
+"""Dataframe for the histogram"""
 
 #We filter out the models of the cars, and keeping solely the make of the car
 make_vehicle = vehicle_df[['model', 'transmission']].copy()
 make_vehicle['model'] = make_vehicle['model'].str.split().str[0]
 
-#Now we are going to group by the model of the vehicle and count the number of occurence that appear in the model index
-grouped_make = make_vehicle[make_vehicle['transmission'] == 'manual'].groupby('model').size().sort_values(ascending=False)
-
 #Create histogram
 fig_histogram = px.histogram(make_vehicle, x='model', title='The Number of Manual Transmission Offered by Makers of Vehicles', labels={'model': 'Car Model', 'count': 'Number of Available Manual Transmissions'})
+
+"""Dataframe for the scatterplot"""
+
+#Now we are going to group by the model of the vehicle and count the number of occurence that appear in the model index
+grouped_make = make_vehicle[make_vehicle['transmission'] == 'manual'].groupby('model').size().sort_values(ascending=False)
 
 #Turning our grouped series into a dataframe so I can use the indexes as axes on the scatterplot
 grouped_make_df = grouped_make.reset_index(name='manuals_offered')
