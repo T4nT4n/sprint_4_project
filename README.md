58f12777010ae4a62357642d8eb87ee2bc57d956
T4nT4n <tannerlawson99@gmail.com>
2024-09-13 19:06:21 -0600

    Second Submission Edit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-This is a web application that displays basic results of EDA of the Vehicles Dataframe.
+Project Title: Sprint 4 Project
 
-Link for Render Web App: https://sprint-4-project-82e6.onrender.com/
+Project Description: This is a web application displaying analysis of a CSV comprising of data of motor vehicles such as odometer readings, model, and more.
+
+Running the project: https://sprint-4-project-82e6.onrender.com/ (simply click the link)
+
+Student Comments:
+- So I need to format the entire dataframe? I think I only formatted the columns that I used for analysis.
+- For the odometer column, it's difficult to tell what values to replace for the null values. I can't use zero because zero is already present in the column. Moreover, whenever I filter the dataframe with cars that have 0 for the odometer column, it appears to be random. Thus, I cannot establish a pattern that indicates what I could substitute for the null values in the odometer. I may add a -1 to indicate that it was missing.
--- app.py
+++ app.py
@@ -9,24 +9,22 @@ st.header("Exploratory Data Analysis")
 
 #Since I only want the make of a vehicle and not the model, I am going to filter out the model column that truly only contains the make--not including the model
 make_vehicle = vehicle_df.copy()
+
+#For this problem, we only care about the model and transmission columns so we should keep only these columns; we also filter for manual transmissions
+make_vehicle = make_vehicle.loc[make_vehicle['transmission'] == 'manual', ['model', 'transmission']]
+
+#We filter the model the so that the make is the only thing in the model column
 make_vehicle['model'] = make_vehicle['model'].str.split().str[0]
 
-#For this problem, we only care about the model and transmission columns so we should keep only these columns
-make_vehicle = make_vehicle[['model', 'transmission']]
-
-#In addition, we only care about manual transmissions so
-make_vehicle = make_vehicle[make_vehicle['transmission'] == 'manual']
-
 #Now we check to see which models offer the most amount of manual transmissions
-grouped_make = make_vehicle.groupby('model').value_counts().sort_values(ascending=False)
+grouped_make = make_vehicle.groupby('model').show()
 
 #Create histogram
 fig_histogram = px.histogram(make_vehicle, x='model', title='The Number of Manual Transmission Offered by Makers of Vehicles', labels={'model': 'Car Model', 'count': 'Number of Available Manual Transmissions'})
 
 #Turning our grouped series into a dataframe so I can use the indexes as axes on the scatterplot
 grouped_make_df = grouped_make.reset_index(name='manuals_offered')
-fig_scatter = px.scatter(grouped_make_df, x='model', y='manuals_offered', labels={'model': 'Car Make', 'manuals_offered': 'Available Manual Transmissions'}, title="The Number of Available Transmissions that Car Makes Provide")
-
+fig_scatter = px.scatter(grouped_make_df, x='model', y='manuals_offered', labels={'model': 'Car Make', 'manuals_offered': 'Available Manual Transmissions'})
 
 # Add a checkbox to show/hide the histogram
 show_histogram = st.checkbox('Show Histogram')
