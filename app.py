977eeea307d9dc8cda2349e1a5c1e373af079f0a
T4nT4n <tannerlawson99@gmail.com>
2024-09-14 08:41:55 -0600

    Didnt realize triple quote comments give headers using streamlit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ vehicle_df = pd.read_csv('vehicles_us.csv')
 # Adding a header
 st.header("Exploratory Data Analysis")
 
-"""Formatting the CSV File"""
+#Formatting the CSV File
 
 #Let's fill the null values of the model year column with zero and convert to integer type
 vehicle_df['model_year'] = vehicle_df['model_year'].fillna(0).astype(int)
@@ -29,7 +29,7 @@ vehicle_df['is_4wd'] = vehicle_df['is_4wd'].fillna(0).astype(int)
 #Let's convert the date_posted column into datetime type
 vehicle_df['date_posted'] = pd.to_datetime(vehicle_df['date_posted'])
 
-"""Dataframe for the histogram"""
+#Dataframe for the Historgram
 
 #We filter out the models of the cars, and keeping solely the make of the car
 make_vehicle = vehicle_df[['model', 'transmission']].copy()
@@ -38,7 +38,7 @@ make_vehicle['model'] = make_vehicle['model'].str.split().str[0]
 #Create histogram
 fig_histogram = px.histogram(make_vehicle, x='model', title='The Number of Manual Transmission Offered by Makers of Vehicles', labels={'model': 'Car Model', 'count': 'Number of Available Manual Transmissions'})
 
-"""Dataframe for the scatterplot"""
+#Dataframe for the Scatter plot
 
 #Now we are going to group by the model of the vehicle and count the number of occurence that appear in the model index
 grouped_make = make_vehicle[make_vehicle['transmission'] == 'manual'].groupby('model').size().sort_values(ascending=False)
